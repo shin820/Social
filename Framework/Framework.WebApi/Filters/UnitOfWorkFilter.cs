4d6941c383d047ffa1c876b7f524457d23be4a76
shin liu <shin820@163.com>
2017-07-13 11:13:08 +0800

    update

--- Framework/Framework.WebApi/Filters/UnitOfWorkFilter.cs
+++ Framework/Framework.WebApi/Filters/UnitOfWorkFilter.cs
@@ -24,7 +24,7 @@ namespace Framework.WebApi.Filters
 
         public async Task<HttpResponseMessage> ExecuteActionFilterAsync(HttpActionContext actionContext, CancellationToken cancellationToken, Func<Task<HttpResponseMessage>> continuation)
         {
-            using (var uow = _uowManager.Begin(new UnitOfWorkOptions { IsTransactional=false}))
+            using (var uow = _uowManager.Begin())
             {
                 var result = await continuation();
                 uow.Complete();
