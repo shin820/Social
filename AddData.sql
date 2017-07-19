7bc368c30c0a81ef9ef16344221aefbb2843d969
shin liu <shin820@163.com>
2017-07-19 10:58:35 +0800

    update

--- AddData.sql
+++ AddData.sql
@@ -3,29 +3,27 @@ INSERT INTO  [Social].[dbo].[t_Social_ConversationField]
 	   [IfSystem]
       ,[DataType]
       ,[Name]
-      ,[CreatedTime]
-      ,[CreatedBy]
       ,[SiteId]
 )
 VALUES
-(1,4,'Source',GETDATE(),1,10000),
-(1,4,'Agent Assignee',GETDATE(),1,10000),
-(1,4,'Department Assignee',GETDATE(),1,10000),
-(1,4,'Status',GETDATE(),1,10000),
-(1,4,'Priority',GETDATE(),1,10000),
-(1,4,'To',GETDATE(),1,10000),
-(1,4,'Replied Agents',GETDATE(),1,10000),
-(1,4,'Last Replied Agent',GETDATE(),1,10000),
-(1,4,'Last Message Sent by',GETDATE(),1,10000),
-(1,4,'Agent Assignee Status',GETDATE(),1,10000),
-(1,4,'Tags',GETDATE(),1,10000),
-(1,1,'From',GETDATE(),1,10000),
-(1,1,'Notes',GETDATE(),1,10000),
-(1,1,'Logs',GETDATE(),1,10000),
-(1,1,'Messages',GETDATE(),1,10000),
-(1,3,'Last Message Sent',GETDATE(),1,10000),
-(1,3,'Created',GETDATE(),1,10000),
-(1,3,'Last Modified',GETDATE(),1,10000),
-(1,2,'Handling Time',GETDATE(),1,10000),
-(1,2,'Total Messages',GETDATE(),1,10000),
-(1,2,'Conversation ID',GETDATE(),1,10000)
+(1,4,'Source',10000),
+(1,4,'Agent Assignee',10000),
+(1,4,'Department Assignee',10000),
+(1,4,'Status',10000),
+(1,4,'Priority',10000),
+(1,4,'To',10000),
+(1,4,'Replied Agents',10000),
+(1,4,'Last Replied Agent',10000),
+(1,4,'Last Message Sent by',10000),
+(1,4,'Agent Assignee Status',10000),
+(1,4,'Tags',10000),
+(1,1,'From',10000),
+(1,1,'Notes',10000),
+(1,1,'Logs',10000),
+(1,1,'Messages',10000),
+(1,3,'Last Message Sent',10000),
+(1,3,'Created',10000),
+(1,3,'Last Modified',10000),
+(1,2,'Handling Time',10000),
+(1,2,'Total Messages',10000),
+(1,2,'Conversation ID',10000)
