2caa6bfd67b1e3b569b281d9d9110d342b98d981
shin liu <shin820@163.com>
2017-08-04 16:11:19 +0800

    update

---
+++ Social.IntegrationTest/DomainServices/Twitter/TweetTreeWalkerTest.cs
@@ -0,0 +1,28 @@
+﻿using Social.Domain.DomainServices.Twitter;
+using Social.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tweetinvi;
+using Tweetinvi.Models;
+using Xunit;
+
+namespace Social.IntegrationTest.DomainServices.Twitter
+{
+    public class TweetTreeWalkerTest : TestBase
+    {
+        [Fact]
+        public void ShouldOnlyWalk10LevelWhenBuildTweetTree()
+        {
+            TweetTreeWalker walker = new TweetTreeWalker();
+            Auth.SetCredentials(new TwitterCredentials(AppSettings.TwitterConsumerKey, AppSettings.TwitterConsumerSecret,
+              "880620227460866048-E1XTzWOFE1xLfxYDor5I5oGMiVUVs86", "ii3mahfjH0qjU4wupnj3Du6JbWjwPuM2p8jHoUrcgpLYp"));
+            var tweet = Tweet.GetTweet(893369057428226049);
+            var tweets = walker.BuildTweetTree(tweet);
+
+            Assert.True(tweets.Count <= 10);
+        }
+    }
+}
---
+++ Social.WebApi/Hubs/SocialHub.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNet.SignalR;
+using Social.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+
+namespace Social.WebApi.Hubs
+{
+    public class SocialHub : Hub
+    {
+        public void NewConversation(Conversation conversation)
+        {
+
+        }
+
+        public void NewMessage(Message message)
+        {
+
+        }
+
+        public override Task OnConnected()
+        {
+            Groups.Add(Context.ConnectionId, Clients.Caller.siteId);
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            Groups.Remove(Context.ConnectionId, Clients.Caller.siteId);
+            return base.OnDisconnected(stopCalled);
+        }
+
+        public override Task OnReconnected()
+        {
+            Groups.Add(Context.ConnectionId, Clients.Caller.siteId);
+            return base.OnReconnected();
+        }
+    }
+}
