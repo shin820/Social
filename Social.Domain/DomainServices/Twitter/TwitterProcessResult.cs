cd3ffc43fb090ddb50cf5d4177f02896bbbf6425
shin820 <shin820@163.com>
2017-08-06 18:00:09 +0800

    update

---
+++ Social.Domain/DomainServices/Twitter/TwitterProcessResult.cs
@@ -0,0 +1,21 @@
+﻿using Social.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Social.Domain.DomainServices.Twitter
+{
+    public class TwitterProcessResult
+    {
+        public Conversation ConversationCreated { get; set; }
+        public Conversation ConversationUpdated { get; set; }
+        public IList<Message> MessagesCreated { get; set; }
+
+        public TwitterProcessResult()
+        {
+            MessagesCreated = new List<Message>();
+        }
+    }
+}
