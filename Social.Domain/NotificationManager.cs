7f257ce9a30d45cbdab6da5c56e72759ee81123e
shin liu <shin820@163.com>
2017-08-14 11:38:57 +0800

    UPDATE

--- Social.Domain/NotificationManager.cs
+++ Social.Domain/NotificationManager.cs
@@ -11,63 +11,59 @@ namespace Social.Infrastructure
     {
         public async Task NotifyNewConversation(int siteId, int conversationId)
         {
-            HttpClient client = CreateHttpClient();
-            await client.GetAsync($"/api/notifications/conversation-created?siteId={siteId}&conversationId={conversationId}");
+            await GetAsync($"/api/notifications/conversation-created?siteId={siteId}&conversationId={conversationId}");
         }
 
         public async Task NotifyUpdateConversation(int siteId, int conversationId)
         {
-            HttpClient client = CreateHttpClient();
-            await client.GetAsync($"/api/notifications/conversation-updated?siteId={siteId}&conversationId={conversationId}");
+            await GetAsync($"/api/notifications/conversation-updated?siteId={siteId}&conversationId={conversationId}");
         }
 
         public async Task NotifyNewFacebookComment(int siteId, int messageId)
         {
-            HttpClient client = CreateHttpClient();
-            await client.GetAsync($"/api/notifications/facebook-comment-created?siteId={siteId}&messageId={messageId}");
+            await GetAsync($"/api/notifications/facebook-comment-created?siteId={siteId}&messageId={messageId}");
         }
 
         public async Task NotifyNewFacebookMessage(int siteId, int messageId)
         {
-            HttpClient client = CreateHttpClient();
-            await client.GetAsync($"/api/notifications/facebook-message-created?siteId={siteId}&messageId={messageId}");
+            await GetAsync($"/api/notifications/facebook-message-created?siteId={siteId}&messageId={messageId}");
         }
 
         public async Task NotifyNewTwitterTweet(int siteId, int messageId)
         {
-            HttpClient client = CreateHttpClient();
-            await client.GetAsync($"/api/notifications/twitter-tweet-created?siteId={siteId}&messageId={messageId}");
+            await GetAsync($"/api/notifications/twitter-tweet-created?siteId={siteId}&messageId={messageId}");
         }
 
         public async Task NotifyNewTwitterDirectMessage(int siteId, int messageId)
         {
-            HttpClient client = CreateHttpClient();
-            await client.GetAsync($"/api/notifications/twitter-direct-message-created?siteId={siteId}&messageId={messageId}");
+            await GetAsync($"/api/notifications/twitter-direct-message-created?siteId={siteId}&messageId={messageId}");
         }
 
         public async Task NotifyNewPublicFilter(int siteId, int filterId)
         {
-            HttpClient client = CreateHttpClient();
-            await client.GetAsync($"/api/notifications/public-filter-created?siteId={siteId}&filterId={filterId}");
+            await GetAsync($"/api/notifications/public-filter-created?siteId={siteId}&filterId={filterId}");
         }
 
         public async Task NotifyDeletePublicFilter(int siteId, int filterId)
         {
-            HttpClient client = CreateHttpClient();
-            await client.GetAsync($"/api/notifications/public-filter-deleted?siteId={siteId}&filterId={filterId}");
+            await GetAsync($"/api/notifications/public-filter-deleted?siteId={siteId}&filterId={filterId}");
         }
 
         public async Task NotifyUpdatePublicFilter(int siteId, int filterId)
         {
-            HttpClient client = CreateHttpClient();
-            await client.GetAsync($"/api/notifications/public-filter-updated?siteId={siteId}&filterId={filterId}");
+            await GetAsync($"/api/notifications/public-filter-updated?siteId={siteId}&filterId={filterId}");
         }
 
-        private HttpClient CreateHttpClient()
+        public async Task GetAsync(string url)
         {
             HttpClient client = new HttpClient();
             client.BaseAddress = new Uri(AppSettings.NotificationApiBaseAddress);
-            return client;
+            var response = await client.GetAsync(url);
+            if (!response.IsSuccessStatusCode)
+            {
+                string msg = await response.Content.ReadAsStringAsync();
+                Logger.Error(msg, null);
+            }
         }
     }
 }
