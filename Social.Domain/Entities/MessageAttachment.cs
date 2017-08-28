dae8f4b29d5705f85b7c85f7f34d180be51d307c
shin liu <shin820@163.com>
2017-08-28 13:06:28 +0800

    update

--- Social.Domain/Entities/MessageAttachment.cs
+++ Social.Domain/Entities/MessageAttachment.cs
@@ -11,7 +11,7 @@ using System.Threading.Tasks;
 namespace Social.Domain.Entities
 {
     [Table("t_Social_MessageAttachment")]
-    public class MessageAttachment : EntityWithSite, IShardingBySiteId
+    public class MessageAttachment : Entity, IShardingBySiteId
     {
         public int MessageId { get; set; }
         [MaxLength(200)]
