bd6fd8dff22095e823b687ed10e57c6d66249cd4
shin liu <shin820@163.com>
2017-09-04 13:42:13 +0800

    update

---
+++ Social.Domain/Entities/Department.cs
@@ -0,0 +1,14 @@
+﻿using Framework.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Social.Domain.Entities
+{
+    public class Department : Entity
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ Social.Infrastructure/Enum/DepartmentStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Social.Infrastructure.Enum
+{
+    public enum DepartmentStatus
+    {
+        Online = 0,
+        Offline = 2,
+    }
+}
