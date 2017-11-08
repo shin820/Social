6cfda26860da9e87214353377b8f1bec17d43255
Vico <Vico@comm100.com>
2017-11-08 11:29:03 +0800

    change index

--- SQL/general-scripts/09 General_Add_t_Social_FilterCondition_Config.sql
+++ SQL/general-scripts/09 General_Add_t_Social_FilterCondition_Config.sql
@@ -12,12 +12,12 @@ BEGIN
 	VALUES
 	('My Open','Status',2,'4',1),
 	('My Open','Agent Assignee',1,'@Me',2),
-	('My Departments'' Open','Status',2,'4',3),
-	('My Departments'' Open','Department Assignee',1,'@My Department',4),
-	('My Offline Department Members'' Open','Status',2,'4',5),
-	('My Offline Department Members'' Open','Agent Assignee',1,'@My Department Member',6),
-	('My Offline Department Members'' Open','Agent Assignee Status',1,'2',7),
-	('All Open','Status',2,'4',8),
-	('Unassigned','Agent Assignee',1,'Blank',9),
-	('Unassigned','Department Assignee',1,'Blank',10)
+	('My Departments'' Open','Status',2,'4',1),
+	('My Departments'' Open','Department Assignee',1,'@My Department',2),
+	('My Offline Department Members'' Open','Status',2,'4',1),
+	('My Offline Department Members'' Open','Agent Assignee',1,'@My Department Member',2),
+	('My Offline Department Members'' Open','Agent Assignee Status',1,'2',3),
+	('All Open','Status',2,'4',1),
+	('Unassigned','Agent Assignee',1,'Blank',1),
+	('Unassigned','Department Assignee',1,'Blank',2)
 END
